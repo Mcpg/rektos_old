91f04b98d4524509d1c38c99d0b7f4c4e18dfae9
Paweł Cholewa <MCPlayG@wp.pl>
2018-09-04 21:31:10 +0200

    Create base for .NET Core rewrite of RektOS 0.4

---
+++ rektos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RektOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RektOS 0.4.0");
+        }
+    }
+}
